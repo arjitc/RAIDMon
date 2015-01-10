8d9a44680198ebcf4789646bf801cf48c022cde9
Arjit Chaudhary <arjit.c@gmail.com>
2015-01-11 01:58:44 +0530

    Update raidmon.sh

--- raidmon.sh
+++ raidmon.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+ACTION=$1
+if [[ -z "$ACTION" ]]
+then
+   echo "Error: action is undefined"
+   echo "Example:"
+   echo "List all drives: sh raidmon.sh listall"
+   exit
+fi
+if [ "$ACTION" == listall ]; then
+	# list all physical drives
+	physical_drives= $(hpacucli ctrl all show config | grep physicaldrive)
+	if [[ -z "physical_drives" ]] then
+	echo "No Physical drives found"
+fi
+if [ "$ACTION" == rebuild status ]; then
+	## RAID rebuild %
+	rebuild_status = $(hpacucli ctrl all show config | grep Recovering | awk {'print $8'})
+	echo $rebuild_status
+	if [[ -z "$rebuild_status" ]] then
+	echo "RAID Status OK"
+fi
 
 
-## RAID rebuild %
-hpacucli ctrl all show config | grep Recovering | awk {'print $8'}
