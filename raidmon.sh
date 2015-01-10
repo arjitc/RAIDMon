98a8e60d29d4a318ff4c4b4ec18bb131cdeb3a9a
Arjit Chaudhary <arjit.c@gmail.com>
2015-01-11 02:11:21 +0530

    Update raidmon.sh

--- raidmon.sh
+++ raidmon.sh
@@ -9,14 +9,14 @@ then
 fi
 if [ "$ACTION" == listall ]; then
 	# list all physical drives
-	physical_drives= $(hpacucli ctrl all show config | grep physicaldrive)
-	if [[ -z "physical_drives" ]]; then
+	physical_drives=$(hpacucli ctrl all show config | grep physicaldrive)
+	if [[ -z "$physical_drives" ]]; then
 	echo "No Physical drives found"
 	fi
 fi
 if [ "$ACTION" == rebuildstatus ]; then
 	## RAID rebuild %
-	rebuild_status = $(hpacucli ctrl all show config | grep Recovering | awk {'print $8'})
+	rebuild_status=$(hpacucli ctrl all show config | grep Recovering | awk {'print $8'})
 	echo $rebuild_status
 	if [[ -z "$rebuild_status" ]]; then
 	echo "RAID Status OK"
