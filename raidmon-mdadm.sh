ab75aaf5a4fa4b864e643ed74314f34d7ea0dd45
Arjit Chaudhary <arjit.c@gmail.com>
2016-01-30 18:12:20 +0530

    Update raidmon-mdadm.sh

--- raidmon-mdadm.sh
+++ raidmon-mdadm.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-ACTION=$1
 ## Example: md0
-mdadm_device=""
+mdadm_device=$1
+ACTION=$2
 
 if [[ -z "$ACTION" ]]
 then
    echo "Error: action is undefined"
    echo "Example:"
-   echo "List all drives: sh raidmon.sh listall"
+   echo "List all drives: sh raidmon.sh md0 listall"
    exit
 fi
+if [[ -z "$mdadm_device" ]]
+then
+   echo "Error: mdadm device is undefined"
+   echo "Example:"
+   echo "List all drives: sh raidmon.sh md0 listall"
+   exit
+fi
+
 if [ "$ACTION" == wd ]; then
 	# Working Devices
 	working_devices=$(mdadm --detail /dev/$mdadm_device | grep "Working Devices" | awk '{print $3}')
@@ -22,10 +30,20 @@ fi
 
 if [ "$ACTION" == listall ]; then
 	# List devices
-	list_devices=$(mdadm --detail /dev/md0 | grep "/dev/")
+	list_devices=$(mdadm --detail /dev/$mdadm_device | grep "/dev/")
 	if [[ -z "$list_devices" ]]; then
 		echo "No Devices found"
 	else
 		echo "$list_devices"
+	fi
+fi
+
+if [ "$ACTION" == getuuid ]; then
+	# List devices
+	get_uuid=$(mdadm --detail /dev/$mdadm_device | grep UUID | awk '{print $3}')
+	if [[ -z "$get_uuid" ]]; then
+		echo "No UUID found"
+	else
+		echo "$get_uuid"
 	fi
 fi
